3c193bb74a5417e26dc9bacbc80ef5e088ea67c2
Ignacio <jjivc10@gmail.com>
2025-01-27 01:00:10 +0000

    Lets gooo

---
+++ Hola.py
@@ -0,0 +1,17 @@
+# Hola Mundo
+
+
+const dogBreeds = [
+  "Labrador Retriever",
+  "German Shepherd",
+  "Golden Retriever",
+  "Bulldog",
+  "Beagle",
+  "Poodle",
+  "Rottweiler",
+  "Yorkshire Terrier",
+  "Boxer",
+  "Dachshund"
+];
+
+
